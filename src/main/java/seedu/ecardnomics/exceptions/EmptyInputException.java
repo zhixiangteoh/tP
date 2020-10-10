776a05b594406d269a92a2e7035383cd939b745b
wangwaynesg <wangwaynesg@gmail.com>
2020-10-10 18:06:00 +0800

    Fix whitespace coding violation

--- src/main/java/seedu/ecardnomics/exceptions/EmptyInputException.java
+++ src/main/java/seedu/ecardnomics/exceptions/EmptyInputException.java
@@ -1,6 +1,6 @@
 package seedu.ecardnomics.exceptions;
 
-public class EmptyInputException extends Exception{
+public class EmptyInputException extends Exception {
     public static final String EMPTY_INPUT_LINE =
             "Input shouldn't be empty! Returning...";
 
