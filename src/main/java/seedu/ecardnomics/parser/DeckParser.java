364f4755bef2c0cdf665e9131371a315e9a74c37
Zhixiang <zhixiangteoh@hotmail.com>
2020-10-14 11:16:34 +0800

    add assertions and logging to DeckParser

--- src/main/java/seedu/ecardnomics/parser/DeckParser.java
+++ src/main/java/seedu/ecardnomics/parser/DeckParser.java
@@ -15,17 +15,26 @@ import seedu.ecardnomics.exceptions.IndexFormatException;
 import seedu.ecardnomics.deck.FlashCard;
 import seedu.ecardnomics.exceptions.EmptyInputException;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DeckParser extends Parser {
     public Deck deck;
+    private static Logger logger = Logger.getLogger("DeckParserLogger");
 
-    /** Constructor. */
+    /**
+     * Constructor.
+     */
     public DeckParser(Deck deck) {
         this.deck = deck;
     }
 
     protected boolean prepareDeletedFlashCard(int flashCardID) {
+        logger.log(Level.INFO, "Retrieving flash card at flash card index");
         FlashCard flashCard = deck.get(flashCardID);
+        assert flashCard != null : "flash card does not exist!";
         String response = getDeleteYorNResponse(flashCard);
+        assert response.equals(Ui.Y) || response.equals(Ui.N) : "response not 'Y' or 'N'!";
         switch (response) {
         case Ui.Y:
             Ui.printFlashCardDeletedLine(flashCard);
@@ -35,6 +44,7 @@ public class DeckParser extends Parser {
             //
             break;
         default:
+            logger.log(Level.SEVERE, "Response should only be either 'Y' or 'N' here");
             //
         }
         return false;
@@ -44,15 +54,21 @@ public class DeckParser extends Parser {
         String[] questionAndAnswer = new String[2];
         Ui.printAddFlashCardLine(deck);
         Ui.printEnterQuestionLine();
+        logger.log(Level.INFO, "Reading user input for question");
         questionAndAnswer[0] = Ui.readUserInput();
         if (questionAndAnswer[0].trim().length() == 0) {
+            logger.log(Level.WARNING, "User entered nothing or a series of blank spaces for question");
             throw new EmptyInputException();
         }
         Ui.printEnterAnswerLine();
+        logger.log(Level.INFO, "Reading user input for answer");
         questionAndAnswer[1] = Ui.readUserInput();
         if (questionAndAnswer[1].trim().length() == 0) {
+            logger.log(Level.WARNING, "User entered nothing or a series of blank spaces for answer");
             throw new EmptyInputException();
         }
+        assert questionAndAnswer[0].length() <= 0 : "question field empty!";
+        assert questionAndAnswer[1].length() <= 0 : "answer field empty!";
         Ui.printFlashCardAddedLine();
         Ui.printDashLines();
 
@@ -62,11 +78,14 @@ public class DeckParser extends Parser {
     @Override
     protected int getIndex(String arguments) throws IndexFormatException, FlashCardRangeException {
         if (!arguments.matches(Ui.DIGITS_REGEX)) {
+            logger.log(Level.WARNING, "User did not enter valid integer");
             throw new IndexFormatException();
         }
+        assert arguments.length() > 0 : "arguments empty!";
         int index = Integer.parseInt(arguments) - Ui.INDEX_OFFSET;
-
+        assert index >= Ui.INDEX_OFFSET : "index argument less than lowest possible index!";
         if (index >= deck.size()) {
+            logger.log(Level.WARNING, "Flash card index larger than total number of cards");
             throw new FlashCardRangeException();
         }
 
@@ -74,6 +93,7 @@ public class DeckParser extends Parser {
     }
 
     protected String getDeleteYorNResponse(FlashCard flashCard) {
+        logger.log(Level.INFO, "Prompting user for 'Y' or 'N' response");
         String response = "";
         do {
             Ui.printDeleteFlashCardLine(flashCard);
@@ -86,9 +106,12 @@ public class DeckParser extends Parser {
                 response = Ui.N;
                 break;
             default:
+                logger.log(Level.INFO, "User entered response other than 'Y' or 'N'");
                 Ui.printInvalidYorNResponse();
+                logger.log(Level.INFO, "Re-prompting...");
             }
         } while (!response.trim().equals(Ui.Y) && !response.trim().equals(Ui.N));
+        assert response.length() == 1 : "response is not 'Y' or 'N'!";
         return response;
     }
 
@@ -99,45 +122,58 @@ public class DeckParser extends Parser {
         switch (commandWord) {
         // Exit
         case Ui.EXIT:
+            logger.log(Level.INFO, "returning ExitCommand object");
             return new ExitCommand();
         // Done with Edit Mode
         case Ui.DONE:
+            logger.log(Level.INFO, "returning DoneEditCommand object");
             return new DoneEditCommand(deck);
         // Add a FlashCard
         case Ui.ADD:
+            logger.log(Level.INFO, "Preparing FlashCard to add");
             String[] questionAndAnswer = prepareFlashCard();
+            logger.log(Level.INFO, "returning AddCommand object");
             return new AddCommand(deck, questionAndAnswer[0], questionAndAnswer[1]);
         // List all FlashCards
         case Ui.LIST:
+            logger.log(Level.INFO, "returning ListCommand object");
             return new ListCommand(deck, arguments);
         // Delete a FlashCard
         case Ui.DELETE:
+            logger.log(Level.INFO, "Preparing FlashCard to delete");
             int flashCardID = getIndex(arguments);
+            assert flashCardID >= Ui.INDEX_OFFSET : "flash card ID less than lowest possible flash card index!";
             boolean isFlashCardDeleted = prepareDeletedFlashCard(flashCardID);
+            logger.log(Level.INFO, "returning DeleteCommand object");
             return new DeleteCommand(deck, flashCardID, isFlashCardDeleted);
         // Help
         case Ui.HELP:
+            logger.log(Level.INFO, "returning HelpCommand object");
             return new HelpCommand();
         default:
+            logger.log(Level.WARNING, "returning VoidCommand object");
             return new VoidCommand();
         }
     }
 
     @Override
     public Command parse(String userInput) {
+        logger.log(Level.INFO, "Parsing user input string");
         String[] splitString = userInput.split(" ", 2);
         String commandWord = splitString[0];
+        logger.log(Level.INFO, "Parsed commandWord");
         String arguments = "";
-        boolean argumentsExist =  splitString.length > 1;
-
+        boolean argumentsExist = splitString.length > 1;
         if (argumentsExist) {
             arguments = splitString[1];
+            logger.log(Level.INFO, "Parsed arguments");
         }
-
+        assert arguments.length() > 0 : "arguments exist but is empty!";
         try {
+            logger.log(Level.INFO, "Parsing command");
             return parseCommand(commandWord, arguments);
-
         } catch (Exception e) {
+            logger.log(Level.WARNING, "Parsed void or invalid command");
             return new VoidCommand(e.getMessage());
         }
     }
