274fb7a5da36f944d5f31c23e1fe95a1d975ef2f
liewws <liewws98@gmail.com>
2020-10-20 23:22:02 +0800

    add logic to format question and answer string for printing

--- src/main/java/seedu/ecardnomics/deck/FlashCard.java
+++ src/main/java/seedu/ecardnomics/deck/FlashCard.java
@@ -1,5 +1,7 @@
 package seedu.ecardnomics.deck;
 
+import seedu.ecardnomics.Ui;
+
 /**
  * Flashcards that contain a question and asnwer.
  */
@@ -55,19 +57,65 @@ public class FlashCard {
 
     @Override
     public String toString() {
-        return "Question: " + question + "\n"
-                + "   Answer:   " + answer;
+        return "Question: " + formatResponse("question") + "\n"
+                + "   Answer:   " + formatResponse("answer");
     }
 
     public String toString(String type) {
+        assert type.equals("question") || type.equals("answer") : "Only two cases here";
         switch (type) {
         case "question":
-            return "Question: " + question;
+            return "Question: " + formatResponse("question");
         case "answer":
-            return "Question: " + question + "\n"
-                    + "   Answer:   " + answer;
+            return "Question: " + formatResponse("question") + "\n"
+                    + "   Answer:   " + formatResponse("answer");
         default:
             return "";
         }
+    }
+
+    /**
+     * Format the question and answer response string to properly
+     * wrap around the end of each line.
+     * IMPORTANT: We assume the following format for printing
+     * Questions: "n. Question: [qn]"
+     * Answers: "   Answer:   [ans]"
+     *
+     * @param type String that should only have value "question" or "answer"
+     * @return String that stores the formatted question or answer
+     */
+    private String formatResponse(String type) {
+        assert type.equals("question") || type.equals("answer") : "Only two cases here";
+        String result = "";
+        int startLength;
+        String[] words;
+        int lineLength = Ui.DASH_LINES.length();
+
+        // We rely on the printing format "n. Question: [qn]"
+        // or "   Answer:   [ans]"
+        if (type.equals("question")) {
+            startLength = "n. Question: ".length();
+            words = question.split(" ");;
+        } else {
+            startLength = "n. Answer:   ".length();
+            words = answer.split(" ");
+        }
+        int usableLength = lineLength - startLength;
+
+        int currentLength = 0;
+        for (String word : words) {
+            currentLength += word.length();
+            if (currentLength > usableLength) {
+                // Repeat enough spaces so that text is aligned to usable area.
+                result += System.lineSeparator() + " ".repeat(startLength) + word;
+                currentLength = word.length();
+            } else {
+                result += word;
+            }
+            result += " ";
+            // Account for the " " after the word.
+            ++currentLength;
+        }
+        return result.trim();
     }
 }
